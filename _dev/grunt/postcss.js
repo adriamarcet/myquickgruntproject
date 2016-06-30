0f0f55bbfbc128aee4912c39786b476a33e3e665
Adrià Marcet <adriamarcetrovira@gmail.com>
2016-06-30 15:21:35 -0400

    Updated to require the plugin ASSETS from postcss
    More documentation here: https://goo.gl/ddEZGQ

--- _dev/grunt/postcss.js
+++ _dev/grunt/postcss.js
@@ -1,6 +1,8 @@
 module.exports = function(grunt) {
+	/* Required the ASSETS postcss plugin */
+	var assets  = require('postcss-assets'); // @https://goo.gl/ddEZGQ
+	
 	grunt.config('postcss', {
-
 		// Autoprefix styles
 		options: {
 			map: true, // inline sourcemaps
